e261f40f2e58e1e5773d853995f06e8bc62b4b1d
Edilson Hdz <edilson.hdz@gmail.com>
2018-10-14 18:15:25 -0600

    tabla editable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { isNumber } from 'util';
 import {InputMaskModule} from 'primeng/inputmask';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'validaciones';
   
   fecha;
   numero;
+  cars = [];
+  min = 0;
+  max = 999999;
+  decimals = 2;
+  format = '$ 0,0.000';
+  rangeValidation = true;
+
+  ngOnInit() {
+    this.cars = [{vin:"valor"}];
+  }
 
   validateDate(inputObject){
     console.log(inputObject)
@@ -39,4 +50,29 @@ export class AppComponent {
   save() {
     console.log(this.fecha);
   }
+
+
+  addnew() {
+    console.log(this.validateAddNew());
+    if (this.validateAddNew() === true) {
+      this.cars.push({vin:""});
+    }    
+  }
+
+  validateAddNew(): boolean {
+    let res = true
+    this.cars.forEach(x => {
+      console.log(x);
+        if (x.vin === '') {
+          res = false;
+        }
+    });
+    return res;
+  }
+
+  print() {
+    console.log(this.cars);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { InputMaskModule } from 'primeng/inputmask';
+import {TableModule} from 'primeng/table';
+
+import { NumericTextboxModule } from 'ngx-numeric-textbox';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,10 @@ import { InputMaskModule } from 'primeng/inputmask';
   imports: [
     BrowserModule,
     FormsModule,
-    InputMaskModule
+    InputMaskModule,
+    TableModule,
+    BrowserAnimationsModule,
+    NumericTextboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
